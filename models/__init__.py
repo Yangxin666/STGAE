f782606bbbf205238cb690b55b7957122afce969
Yangxin F <47265586+Yangxin666@users.noreply.github.com>
2022-08-15 13:18:14 -0400

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,6 @@
+# @Time     : 08/15/2022
+# @Author   : Yangxin Fan
+# @FileName : __init__.py
+# @Version  : 1.0
+# @IDE      : Jupyter Notebook
+# @Github   : https://github.com/Yangxin666/STD-GAE
